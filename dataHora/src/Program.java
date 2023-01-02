7d46d92ab55affec43b65b6da9e647f73f95fbd5
Samuel da Silva <108034309+SamColt36@users.noreply.github.com>
2023-01-02 10:56:52 -0300

    Add files via upload

---
+++ dataHora/src/Program.java
@@ -0,0 +1,73 @@
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		int i;
+
+		DateTimeFormatter f = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter fComHora = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+		LocalDateTime hoje = LocalDateTime.now();
+		System.out.println("Today is " + hoje.format(fComHora) + "h");
+		System.out.println();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter with the date (DD MM YYYY): ");
+		int dia = sc.nextInt();
+		int mes = sc.nextInt();
+		int ano = sc.nextInt();
+
+		LocalDate dataParaBuscar = LocalDate.of(ano, mes, dia);
+		LocalDateTime dataParaBuscarComHora = LocalDateTime.of(ano, mes, dia, 9, 0);
+
+		for (i = 0; i < 365; i = i + 2) {
+			LocalDate inicioDaContagem = LocalDate.parse("2022-12-26");
+			inicioDaContagem = inicioDaContagem.plusDays(i);
+
+			String trab = inicioDaContagem.format(f);
+			String buscar = dataParaBuscar.format(f);
+
+			if (buscar.contentEquals(trab) == true) {
+				if (buscar.contentEquals("01/01/2023") || buscar.contentEquals("25/12/2023")) {
+					System.out.print("folga".toUpperCase());
+					break;
+				} else {
+					Duration inter = Duration.between(hoje, dataParaBuscarComHora);
+					System.out.print("Voce Trabalha em ");
+					conversaoDeTempo((int) inter.toSeconds());
+					break;
+				}
+			}
+
+		}
+
+		for (i = 1; i < 365; i = i + 2) {
+			LocalDate inicioDaContagem = LocalDate.parse("2022-12-26");
+			inicioDaContagem = inicioDaContagem.plusDays(i);
+
+			String folga = inicioDaContagem.format(f);
+			String buscar = dataParaBuscar.format(f);
+
+			if (buscar.contentEquals(folga) == true) {
+				System.out.print("folga".toUpperCase());
+				break;
+			}
+		}
+		sc.close();
+	}
+
+	public static void conversaoDeTempo(int s) {
+		int d, h, m, sg;
+		d = (s / 86400);
+		h = (s % 86400) / 3600;
+		m = ((s % 86400) % 3600) / 60;
+		sg = ((s % 86400) % 3600) % 60;
+		System.out.println(d + " dia(s) " + h + " hora(s) " + m + " minuto(s) " + sg + " segundo(s)");
+	}
+}
